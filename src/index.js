1f27fa58796748a40be9aafaedf4af61ac681468
Sata <35007410+Satpix@users.noreply.github.com>
2020-10-02 21:32:16 +0300

    Feat: Update index.js
    add min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===undefined) return 0;
+  if (array.length === 0) return 0;
+  let littleNum=array[0];
+  for (i=0;i<array.length;i++){
+    if (littleNum>array[i]){
+        littleNum=array[i];
+    }
 }
-
+return littleNum;
+}
 exports.max = function max (array) {
-  return 0;
+  if (array===undefined) return 0;
+  if (array.length === 0) return 0;
+  let bigNum=array[0];
+  for (i=0;i<array.length;i++){
+    if (bigNum<array[i]){
+        bigNum=array[i];
+    }
 }
-
+return bigNum;
+}
 exports.avg = function avg (array) {
-  return 0;
+    if (array===undefined) return 0;
+    if (array.length === 0) return 0;
+    let sum=0;
+    for (i=0;i<array.length;i++){
+        sum+=array[i];
+    }
+    let middleSum=sum/array.length;
+    return middleSum;
 }
